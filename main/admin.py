d26a4719f1f308ed3a56939594adb007c4b9367d
alpallel <alpallel.e@gmail.com>
2026-09-10 16:19:49 +0700

    feat : add admin dashboard for manual CRUD operations

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Projects, Experience 
 
-# Register your models here.
+# Register your model here
+admin.site.register(Projects)
+admin.site.register(Experience)
