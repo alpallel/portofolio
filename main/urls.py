1b3fa9ee1368c7729f4d6c631016374496570a0c
alpallel <alpallel.e@gmail.com>
2026-09-15 14:08:36 +0700

    feat: add API endpoint for project JSON data

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from main.views import show_main, show_experience, show_projects, create_project
+from main.views import show_main, show_experience, show_projects, create_project, get_projects_json
 
 app_name = "main"
 
@@ -9,4 +9,5 @@ urlpatterns = [
     path("experience/", show_experience, name="show_experience"),
     path("projects/", show_projects, name="show_projects"),
     path("projects/add/", create_project, name="create_project"),
+    path("api/projects/", get_projects_json, name="get_projects_json"),
 ]
--- main/views.py
+++ main/views.py
@@ -33,10 +33,20 @@ def show_experience(request):
     return render(request, "experience.html", context)
 
 def show_projects(request):
+    json_response = get_projects_json(request)
+
+    projects = serializers.deserialize(
+        "json",
+        json_response.content.decode("utf-8"),
+    )
+    projects = [project.object for project in projects]
+    title_query = request.GET.get("title", "").strip()
+
     context = {
         "name": "Alfarrel Ersya Balawa",
         "short_name": "Alfarrel",
         "projects_list": Project.objects.all(),
+        "title_query": title_query,
     }
     return render(request, "projects.html", context)
 
@@ -54,3 +64,13 @@ def create_project(request):
         "form": form,
     }
     return render(request, "projects_form.html", context)
+
+def get_projects_json(request):
+    title_query = request.GET.get("title", "").strip()
+    projects = Project.objects.all()
+
+    if title_query:
+        projects = projects.filter(title__icontains=title_query)
+
+    projects_json = serializers.serialize("json", projects)
+    return HttpResponse(projects_json, content_type="application/json")
